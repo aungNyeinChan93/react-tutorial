61951aa5a93a7c6d46fcff3872e7d70331f418b8
aungNyeinChan93 <aungNyeinChan.dev@gmail.com>
2024-05-14 21:02:23 +0630

    add counter/learning useState Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
 import CardList from './components/CardList';
+import Counter from './components/Counter';
 
 const App = () => {
     
@@ -9,6 +10,11 @@ const App = () => {
         <Fragment>
             <div className="app-section">
                 <CardList cardTitle="Card Title" cardDescription = "this is description"/>
+            </div>
+
+            {/* COunter App / useState_learning */}
+            <div className="counter-app" style={{minWidth:"100vw", minHeight:"100vh"}}>
+                <Counter/>
             </div>
         </Fragment>
     )
---
+++ src/components/Counter.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useState } from 'react';
+
+const Counter = () => {
+    let [counter ,setCounter] = useState(0);
+
+    function doubleCounter(){
+        setCounter((preCounter)=>preCounter+1)
+        setCounter((preCounter)=>preCounter+1)
+    }
+    return (
+        <div className='Counter' style={{textAlign:"center", backgroundColor:"gray ",padding:"10px 10px" ,borderRadius:"10px", margin:"10px 50px"}}>
+            <h1 style={{marginTop:"10px"}}>{counter}</h1>
+            <button style={{padding:"10px", }} onClick={()=>setCounter((preCounter)=>preCounter+=1)}>Counter</button>
+            <button onClick={()=>doubleCounter()} style={{padding:"10px",marginLeft:"10px" }} >Double counter</button>
+        </div>
+    )
+}
+
+export default Counter
